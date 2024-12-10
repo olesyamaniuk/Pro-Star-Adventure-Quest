5f35744ba0fb9d3972c498ec12accfde9090fcd5
olesyamaniuk <maniukolesya222@gmail.com>
2024-12-10 11:50:52 +0200

    1

---
+++ js/slick.js
@@ -0,0 +1,77 @@
+$(function () {
+  $('.about-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.about__list-left',
+    nextArrow: '.about__list-right',
+    
+  });
+});
+
+$(function () {
+  $('.gallery__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.gallery__list-left',
+    nextArrow: '.gallery__list-right',
+    
+  });
+});
+$(function () {
+  $('.blog__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.blog__list-left',
+    nextArrow: '.blog__list-right',
+    
+  });
+});
+
+$(function () {
+  $('.reviews__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: false,
+    cssEase: 'linear',
+    prevArrow: '.reviews__list-left',
+    nextArrow: '.reviews__list-right',
+    
+  });
+});
+
+
+
+
+
+
+
